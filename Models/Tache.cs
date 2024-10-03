d1c531c109f2d52acb378617e014ce612a8e5536
craphael <raphael.cau@ynov.com>
2024-10-03 16:25:38 -0700

    error message sur model tache

--- Models/Tache.cs
+++ Models/Tache.cs
@@ -7,11 +7,13 @@ public class Tache
     
     [Key] public int Id { get; set; }
     
+    [Required(ErrorMessage = "Il faut donner un nom a votre tache")]
     public string task { get; set; }
     
     [Required(ErrorMessage = "date de la tache requise")]
     public DateTime date { get; set; }
     
+    [Required(ErrorMessage = "veuillez entrer une categorie")]
     public string categorie { get; set; }
     
     public string description { get; set; }
